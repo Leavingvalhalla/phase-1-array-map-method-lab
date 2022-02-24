250eda3fb242e57a45bfd8d689c27504d4ae44b3
Zach Lefler <leaving.valhalla@gmail.com>
2022-02-24 12:52:02 -0800

    initial commit

--- index.js
+++ index.js
@@ -8,9 +8,23 @@ const tutorials = [
   'Immutable State and Pure Functions',
   'what is the difference between == and ===?',
   'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
+  'what is JSONP?',
 ];
 
-const titleCased = () => {
-  return tutorials
+function titleCased() {
+  // makes an array of arrays, each containing each word as an item
+  const arrays = tutorials.map((item) => item.split(' '));
+  // takes each array(each title) and uses the words function on each array, returning a new array of each array
+  const item = arrays.map((title) => words(title));
+
+  function words(item) {
+    // takes each inner array, takes each item and makes the first character upper case
+    const product = item.map(
+      (word) => word.slice(0, 1).toUpperCase() + word.slice(1)
+    );
+    // returns the array of titles, changing each title back to a string
+    return product.join(' ');
+  }
+
+  return item;
 }
